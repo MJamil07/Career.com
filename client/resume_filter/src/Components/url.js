eace4d3fd5487efada318f0f194964f21951e4d6
Jamil M <89064648+MJamil07@users.noreply.github.com>
2023-08-14 00:58:49 +0530

    Update url.js | added server url

--- client/resume_filter/src/Components/url.js
+++ client/resume_filter/src/Components/url.js
@@ -1,6 +1,7 @@
 
 
-const URL = 'http://localhost:7878'
+// const URL = 'http://localhost:7878'
+const URL = "https://career-com-api.vercel.app"
 // const SERVER_URL = 'C:\\Users\\jamil\\OneDrive\\Documents\\Resume Filter\\server'
 // export { SERVER_URL }
 export default URL
