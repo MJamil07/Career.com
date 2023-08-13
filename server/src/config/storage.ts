3e9d4610c817b853f7797cec7f5061c1ba86bd74
Jamil M <89064648+MJamil07@users.noreply.github.com>
2023-08-14 01:41:39 +0530

    Update storage.ts

--- server/src/config/storage.ts
+++ server/src/config/storage.ts
@@ -1,11 +1,8 @@
-import path from "path";
 import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const destinationPath = path.resolve(process.cwd(), "src/public/pdf");
-    console.log(destinationPath);
-    cb(null, destinationPath);
+    cb(null, "https://github.com/MJamil07/Career.com/tree/main/server/src/public/pdf");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
